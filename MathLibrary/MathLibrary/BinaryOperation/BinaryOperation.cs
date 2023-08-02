4e0eaa9a857c2a51b2fa320b4a099425f608db25
Abhishek Pandey <abhishek.cse24@gmail.com>
2023-08-02 17:32:14 +0530

    created class library and structure of the classes

---
+++ MathLibrary/MathLibrary/BinaryOperation/BinaryOperation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathLibrary
+{
+    internal class BinaryOperation : IOperation
+    {
+        public void Evaluate()
+        {
+            Console.WriteLine("This implements binary Operations");
+        }
+    }
+}
